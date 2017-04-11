e470ea2d751d1bbf9422bc714895c7978597662f
Suntey Kuznetsov <suntey.kuznetsov@gmail.com>
2017-04-11 22:57:33 +0300

    HW3 Optional full MealServiceTest

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -15,6 +15,7 @@ public class MealTestData {
     public static final Meal BREAKFAST = new Meal(MEAL_ID, LocalDateTime.of(2017, Month.MARCH, 1, 10, 00), "Завтрак", 500);
     public static final Meal LUNCH = new Meal(MEAL_ID + 1, LocalDateTime.of(2017, Month.MARCH, 1, 13, 30), "Обед", 1000);
     public static final Meal SUPPER = new Meal(MEAL_ID + 2, LocalDateTime.of(2017, Month.MARCH, 1, 20, 00), "Ужин", 500);
+    public static final Meal ANOTHERDAY = new Meal(MEAL_ID + 3, LocalDateTime.of(2017, Month.MARCH, 2, 10, 00), "Завтрак1", 500);
 
     public static final ModelMatcher<Meal> MEAL_MATCHER = new ModelMatcher<>(
             (expected, actual) -> expected == actual || Objects.equals(expected.toString(), actual.toString()));
--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -9,9 +9,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.DbPopulator;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.Arrays;
+import java.util.List;
 
 import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
@@ -46,26 +49,30 @@ public class MealServiceTest {
         MEAL_MATCHER.assertEquals(BREAKFAST, meal);
     }
 
-    @Test
-    public void delete() throws Exception {
-        service.delete(MEAL_ID, USER_ID);
-        System.out.println(service.getAll(USER_ID));
-        MEAL_MATCHER.assertCollectionEquals(Arrays.asList(SUPPER, LUNCH), service.getAll(USER_ID));
+    @Test(expected = NotFoundException.class)
+    public void getNoFound() throws Exception{
+        service.get(123, USER_ID);
     }
 
     @Test
-    public void getBetweenDates() throws Exception {
-
+    public void delete() throws Exception {
+        service.delete(MEAL_ID, USER_ID);
+        MEAL_MATCHER.assertCollectionEquals(Arrays.asList(ANOTHERDAY, SUPPER, LUNCH), service.getAll(USER_ID));
+    }
+    @Test(expected = NotFoundException.class)
+    public void deleteNotFound() throws Exception {
+        service.delete(123, USER_ID);
     }
 
     @Test
     public void getBetweenDateTimes() throws Exception {
-
+        List<Meal> meals= service.getBetweenDateTimes(LocalDateTime.of(2017, Month.MARCH, 2, 9, 0), LocalDateTime.of(2017, Month.MARCH, 3, 9, 0), USER_ID);
+        MEAL_MATCHER.assertCollectionEquals(Arrays.asList(ANOTHERDAY), meals);
     }
 
     @Test
     public void getAll() throws Exception {
-        MEAL_MATCHER.assertCollectionEquals(Arrays.asList(SUPPER, LUNCH, BREAKFAST), service.getAll(USER_ID));
+        MEAL_MATCHER.assertCollectionEquals(Arrays.asList(ANOTHERDAY, SUPPER, LUNCH, BREAKFAST), service.getAll(USER_ID));
     }
 
     @Test
@@ -81,7 +88,8 @@ public class MealServiceTest {
         Meal newMeal = new Meal(LocalDateTime.now(), "newMeal", 1000);
         Meal created = service.save(newMeal, USER_ID);
         newMeal.setId(created.getId());
-        MEAL_MATCHER.assertCollectionEquals(Arrays.asList(newMeal, SUPPER, LUNCH, BREAKFAST), service.getAll(USER_ID));
+        MEAL_MATCHER.assertCollectionEquals(Arrays.asList(newMeal,ANOTHERDAY, SUPPER, LUNCH, BREAKFAST), service.getAll(USER_ID));
     }
+
 
 }
