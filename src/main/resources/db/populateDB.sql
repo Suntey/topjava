8fa685f7bc81c327ee7d6ac9a6de62092868c43a
Suntey Kuznetsov <suntey.kuznetsov@gmail.com>
2017-04-11 17:45:37 +0300

    Homework without optional

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -11,3 +12,10 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals(calories, description, user_id) VALUES ('1200', 'Завтрак', 100000);
+INSERT INTO meals(calories, description, user_id) VALUES ('1500', 'Обед', 100000);
+INSERT INTO meals(calories, description, user_id) VALUES ('2000', 'Ужин', 100000);
+INSERT INTO meals(calories, description, user_id) VALUES ('500', 'Завтрак1', 100000);
+INSERT INTO meals(calories, description, user_id) VALUES ('1000', 'Обед1', 100000);
+INSERT INTO meals(calories, description, user_id) VALUES ('500', 'Ужин1', 100000);
